37656d52246818674bcffe31eae7c6911d41d917
Mister Frank Baidoo <132353054+fbaid@users.noreply.github.com>
2025-04-12 12:45:51 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$secondary_key = "";
+$data = array("providerCallbackHost" => "http://localhost/momo/callback.php");
+$apiUser = "";
+$apiKey = "";
+
+//UUID Version 4
+function generate_uuid(){
+    $data = random_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+     $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+$uuid = generate_uuid();
